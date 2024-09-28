1efbe15d73f85d87fc1534aaba8f4a5ee0707c07
VallyL <levin.valeriia01@gmail.com>
2024-09-28 14:29:51 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
 function App() {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Main />
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Typography</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="md">
+        <Typography variant="h4" gutterBottom>
+          Welcome to my application!
+        </Typography>
+
+        <Button variant="contained" onClick={handleOpenDialog}>
+          Open Modal Window
+        </Button>
+
+        <Dialog open={openDialog} onClose={handleCloseDialog}>
+          <DialogTitle>Modal Window</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Hello!</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </Container>
     </div>
   );
 }
